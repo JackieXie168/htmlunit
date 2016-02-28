4e49a2c0e459a951f8f69a28a1a736e598be260e
asashour <asashour@5f5364db-9458-4db8-a492-e30667be6df6>
2016-02-28 16:09:19 +0000

    Release 2.20

---
+++ tags/HtmlUnit-2.20/src/main/java/com/gargoylesoftware/htmlunit/javascript/host/xml/FormData.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2002-2016 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host.xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.FormEncodingType;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.javascript.SimpleScriptable;
+import com.gargoylesoftware.htmlunit.javascript.configuration.JsxClass;
+import com.gargoylesoftware.htmlunit.javascript.configuration.JsxConstructor;
+import com.gargoylesoftware.htmlunit.javascript.configuration.JsxFunction;
+import com.gargoylesoftware.htmlunit.javascript.host.file.File;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLFormElement;
+import com.gargoylesoftware.htmlunit.util.KeyDataPair;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
+
+import net.sourceforge.htmlunit.corejs.javascript.Context;
+
+/**
+ * A JavaScript object for {@code FormData}.
+ *
+ * @author Ahmed Ashour
+ * @author Ronald Brill
+ */
+@JsxClass
+public class FormData extends SimpleScriptable {
+
+    private final List<NameValuePair> requestParameters_ = new ArrayList<>();
+
+    /**
+     * Default constructor.
+     */
+    public FormData() {
+    }
+
+    /**
+     * Constructor.
+     * @param formObj a form
+     */
+    @JsxConstructor
+    public FormData(final Object formObj) {
+        if (formObj instanceof HTMLFormElement) {
+            final HTMLFormElement form = (HTMLFormElement) formObj;
+            requestParameters_.addAll(form.getHtmlForm().getParameterListForSubmit(null));
+        }
+    }
+
+    /**
+     * Appends a new value onto an existing key inside a {@code FormData} object,
+     * or adds the key if it does not already exist.
+     * @param name the name of the field whose data is contained in {@code value}
+     * @param value the field's value
+     * @param filename the filename reported to the server (optional)
+     */
+    @JsxFunction
+    public void append(final String name, final Object value, final Object filename) {
+        if (value instanceof File) {
+            final File file = (File) value;
+            String fileName = null;
+            if (filename instanceof String) {
+                fileName = (String) filename;
+            }
+            requestParameters_.add(new KeyDataPair(name, file.getFile(), fileName, file.getType(), null));
+        }
+        else {
+            requestParameters_.add(new NameValuePair(name, Context.toString(value)));
+        }
+    }
+
+    /**
+     * Sets the specified request with the parameters in this {@code FormData}.
+     * @param webRequest the web request to fill
+     */
+    public void fillRequest(final WebRequest webRequest) {
+        webRequest.setEncodingType(FormEncodingType.MULTIPART);
+        webRequest.setRequestParameters(requestParameters_);
+    }
+}
