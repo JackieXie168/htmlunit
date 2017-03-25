093c2609f69f5ec8dca16e33c2d21de24a99189e
asashour <asashour@5f5364db-9458-4db8-a492-e30667be6df6>
2017-03-25 21:52:53 +0000

    Fix tests

--- branches/huThread/htmlunit/src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/UIEventTest.java
+++ branches/huThread/htmlunit/src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/UIEventTest.java
@@ -93,13 +93,19 @@ public class UIEventTest extends WebDriverTestCase {
             + "  <div id='a' onclick='alertDetail(event)'>abc</div>\n"
             + "  <div id='b' ondblclick='alertDetail(event)'>xyz</div>\n"
             + "</body></html>";
+
+        final String[] alerts = getExpectedAlerts();
+        int i = 0;
         final WebDriver driver = loadPage2(html);
+        verifyAlerts(driver, alerts[i++], alerts[i++]);
+
         driver.findElement(By.id("a")).click();
+        verifyAlerts(driver, alerts[i++], alerts[i++]);
+
         final Actions action = new Actions(driver);
         action.doubleClick(driver.findElement(By.id("b")));
         action.perform();
-
-        verifyAlerts(driver, getExpectedAlerts());
+        verifyAlerts(driver, alerts[i++], alerts[i++]);
     }
 
     /**
@@ -118,9 +124,13 @@ public class UIEventTest extends WebDriverTestCase {
             + "</script>\n"
             + "<form><input type='button' id='b' onclick='alertView(event)'></form>\n"
             + "</body></html>";
-        final WebDriver driver = loadPage2(html);
-        driver.findElement(By.id("b")).click();
 
-        verifyAlerts(driver, getExpectedAlerts());
+        final String[] alerts = getExpectedAlerts();
+        int i = 0;
+        final WebDriver driver = loadPage2(html);
+        verifyAlerts(driver, alerts[i++], alerts[i++]);
+
+        driver.findElement(By.id("b")).click();
+        verifyAlerts(driver, alerts[i++], alerts[i++]);
     }
 }
